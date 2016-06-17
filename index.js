43564c1d7d0d83deff58afdb27189026c0e8b387
George Adamson <george.adamson@baesystems.com>
2016-06-17 12:43:50 +0100

    v0.0.1

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
-var postcss = require('postcss');
+
+// Postcss api docs: https://github.com/postcss/postcss/blob/master/docs/api.md
+
+const postcss = require('postcss');
+const list    = postcss.list;
+
+function serialiseDeclarations (rule) {
+  var nodes = rule.nodes ? rule.nodes.sort().map(String) : [];
+  return nodes.join(';').replace(/\s+/g,'');
+}
+
+// function intersect (a, b, not) {
+//   return a.filter(c => {
+//     const index = ~b.indexOf(c);
+//     return not ? !index : index;
+//   });
+// }
+
+// function difference (a, b) {
+//   return intersect(a, b, true).concat(intersect(b, a, true));
+// }
+
+// Usage: array.filter(unique)
+function unique (value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function selectorMerger (groupOpts) {
+  const cache = {};
+
+  return function analyseRule (ruleB) {
+
+    const decl = serialiseDeclarations(ruleB);
+
+    if (cache[decl]) {
+
+      const ruleA = cache[decl];
+      const a = list.comma(ruleA.selector);
+      const b = list.comma(ruleB.selector);
+      const mergedSelector = a.concat(b).filter(unique).join(', ');
+
+      if (groupOpts.promote){
+        ruleB.selector = mergedSelector;
+        ruleA.remove();
+        cache[decl] = ruleB;
+      } else {
+        ruleA.selector = mergedSelector;
+        ruleB.remove();
+      }
+
+    } else {
+
+      cache[decl] = ruleB;
+
+    }
+
+    return;
+
+  };
+
+}
 
 module.exports = postcss.plugin('postcss-merge-selectors', function (opts) {
-    opts = opts || {};
 
-    // Work with options here
+  const defaultOptions = {
+    groups : [
+      {
+        selectorFilter : /.*/,  // string|RegExp
+        promote        : false
+      }
+    ]
+  };
 
-    return function (css, result) {
+  opts = opts || defaultOptions;
 
-        // Transform CSS AST here
+  return function (css /* , result */) {
 
-    };
+    const defaultFilter = defaultOptions.groups[0].selectorFilter;
+    const groups = opts.groups && opts.groups.length > 0 ? opts.groups : defaultOptions.groups;
+
+    return groups.forEach(group => {
+      css.walkRules(group.selectorFilter || defaultFilter, selectorMerger(group));
+    })
+
+  };
 });
--- test.js
+++ test.js
@@ -3,18 +3,69 @@ import test    from 'ava';
 
 import plugin from './';
 
-function run(t, input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
-            t.deepEqual(result.css, output);
-            t.deepEqual(result.warnings().length, 0);
-        });
+function run (t, input, expected, opts = { }) {
+  return postcss([ plugin(opts) ]).process(input)
+    .then(result => {
+      t.deepEqual(result.css, expected);
+      t.deepEqual(result.warnings().length, 0);
+    });
 }
 
-/* Write tests here
+// Write tests here
 
-test('does something', t => {
-    return run(t, 'a{ }', 'a{ }', { });
+test('Should merge adjacent selectors that have identical styles', t => {
+  return run(t,
+    '.foo { width:0; height:0 } .bar { height:0; width:0 }',
+    '.foo, .bar { height:0; width:0 }',
+    {}
+  );
 });
 
-*/
+
+test('Should not merge selectors that have different styles', t => {
+  return run(t,
+    '.foo { width:0; height:0 } A.foobar { top: 0 } .bar { height:0; width:0 }',
+    '.foo, .bar { height:0; width:0 } A.foobar { top: 0 }',
+    {}
+  );
+});
+
+
+test('Should merge selectors and dedupe those with same name', t => {
+  return run(t,
+    '.foo { top:0 } .bar { top: 0 } .foo { top:0 }',
+    '.foo, .bar { top:0 }',
+    {}
+  );
+});
+
+test('Should merge selectors that match config group', t => {
+  return run(t,
+    '.foo1 { top:0 } .fooDummy { top:0 } .foo2 { top:0 } .fooDummy { top:0 }',
+    '.foo1, .foo2 { top:0 } .fooDummy { top:0 } .fooDummy { top:0 }',
+    { groups : [
+      { selectorFilter : /\.foo\d+/ }
+    ]}
+  );
+});
+
+test('Should merge selectors at the position of the last occurrence (when flag set)', t => {
+  return run(t,
+    '.foo1 { top:0 } .bar { top:0 } .foo2 { top:0 } .bar { top:0 }',
+    '.bar { top:0 } .foo1, .foo2 { top:0 } .bar { top:0 }',
+    { groups : [
+      { selectorFilter : /\.foo/, promote : true  }
+    ]}
+  );
+});
+
+test('Should merge selectors at the position of the last occurrence (when flag set)', t => {
+  return run(t,
+    '.foo1 { top:0 } .bar1 { top:0 } .foo2 { top:0 } .bar2 { top:0 }',
+    '.bar1, .bar2 { top:0 } .foo1, .foo2 { top:0 }',
+    { groups : [
+      { selectorFilter : /\.foo/, promote : true  },
+      { selectorFilter : /\.bar/, promote : false }
+    ]}
+  );
+});
